05b5eea10ecbc6614e6ce2061f382001ecd74909
Taras-Veremchuk <taras.veremm@gmail.com>
2023-03-04 01:16:35 +0100

    done home-work-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const STORAGE_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(ev => {
+    localStorage.setItem(STORAGE_KEY, ev.seconds);
+  }, 1000)
+);
+
+const playerCurrentTime = localStorage.getItem(STORAGE_KEY);
+
+player
+  .setCurrentTime(playerCurrentTime)
+  .then(function (seconds) {
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name = "email"]'),
+  message: document.querySelector('textarea[name = "message"]'),
+};
+const STORAGE_KEY = 'feedback-form-state';
+const { form, email, message } = refs;
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+reloadPage();
+
+function onFormInput(e) {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (!message.value || !email.value) {
+    alert('Заповніть всі поля');
+  } else {
+    const currentString = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log('Поточне значення форми:', currentString);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+function reloadPage() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage) {
+    console.log(savedMessage);
+    email.value = savedMessage.email;
+    message.value = savedMessage.message;
+  }
+}
